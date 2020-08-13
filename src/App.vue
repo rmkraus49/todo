a2351afc586684a177fc45c8d12ab2cb95c69455
rmkraus49 <60583755+rmkraus49@users.noreply.github.com>
2020-08-13 17:03:20 -0400

    updated styles, delete, edit functionality

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,93 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section>
+    <md-list class="todos">
+      <h1 class="title">To-dos</h1>
+      <md-field>
+        <md-input
+          class="new-item-input"
+          v-model="currentTodo"
+          @keydown.enter="addTodo()"
+          placeholder="Add a to-do"
+        ></md-input>
+      </md-field>
+      <md-list-item v-for="todo in todos" :key="todo.id" :class="{ editing: todo === editedTodo }">
+        <input class="completed-checkbox" type="checkbox" v-model="todo.completed" />
+        <span class="md-list-item-text view-todo" @dblclick="editTodo(todo)">{{ todo.label }}</span>
+        <md-field class="edit-todo">
+          <md-input v-model="todo.label" @keydown.enter="stopEditing()"></md-input>
+        </md-field>
+        <md-button class="md-accent" @click="removeTodo(todo)">Delete</md-button>
+      </md-list-item>
+    </md-list>
+  </section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      todos: [],
+      currentTodo: "",
+      editedTodo: ""
+    };
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({
+        id: this.todos.length,
+        label: this.currentTodo,
+        completed: false
+      });
+      this.currentTodo = "";
+    },
+    removeTodo(todo) {
+      var index = this.todos.indexOf(todo);
+      this.todos.splice(index, 1);
+    },
+    editTodo(todo) {
+      this.editedTodo = todo;
+    },
+    stopEditing() {
+      this.editedTodo = "";
+    }
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss" scoped>
+section {
+  display: flex;
+}
+
+.todos {
+  min-width: 70%;
+  margin: 10px 20px;
+}
+
+.title {
+  margin-left: 20px;
+}
+
+.new-item-input {
+  margin: 20px;
+}
+
+.edit-todo {
+  display: none;
+}
+.editing {
+  font-weight: bold;
+  md-field.edit-todo {
+  }
+  span.view-todo {
+    display: none;
+  }
+  .edit-todo {
+    display: flex;
+  }
+}
+
+.completed-checkbox {
+  margin: 10px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { MdField, MdButton, MdList, MdCheckbox } from 'vue-material/dist/components';
+import 'vue-material/dist/vue-material.min.css';
+import 'vue-material/dist/theme/default.css';
+
+Vue.use(MdField);
+Vue.use(MdButton);
+Vue.use(MdList);
+Vue.use(MdCheckbox);
 
 Vue.config.productionTip = false
 
